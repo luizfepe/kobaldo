0eb71c1fee83ed6afa065d827094a6343fa8f60d
kobkobkob <eduardo@cle.to>
2015-12-04 01:29:26 -0200

    pong 3d 1

---
+++ NovaEra/Assets/Scripts/Bola.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bola : MonoBehaviour
+{
+    public float speed = 30;
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = Vector3.right * speed;
+    }
+}
---
+++ NovaEra/Assets/Scripts/Ps.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ps : MonoBehaviour
+{
+    public float speed = 30;
+
+    public string axis = "Vertical";
+
+    void FixedUpdate()
+        {
+            float v = Input.GetAxisRaw(axis);
+            GetComponent<Rigidbody>().velocity = new Vector3(0,0, v) * speed;
+        }
+    }
